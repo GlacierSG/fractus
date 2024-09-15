9ff47227a110fe3889ec9e7f5445498f8ec46890
GlacierSG <glaciersg@proton.me>
2024-09-15 13:07:29 +0000

    added rsa, rc4 and replacer

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,12 @@
 pub mod hash;
+mod cipher;
+mod error;
 
 #[cfg(feature = "pyo3")]
 pub mod py;
 
-pub use hash::md4;
-pub use hash::md5;
-pub use hash::sha0;
-pub use hash::sha1;
-pub use hash::sha2_224;
-pub use hash::sha2_256;
-pub use hash::sha2_512;
-pub use hash::ripemd128;
-pub use hash::ripemd160;
-pub use hash::ripemd256;
-pub use hash::ripemd320;
-pub use hash::whirlpool;
+pub use error::*;
+
+pub use cipher::*;
+pub use hash::*;
+
--- src/py.rs
+++ src/py.rs
@@ -1,6 +1,15 @@
 use pyo3::prelude::*;
 use crate::hash::py::hash;
+use crate::cipher::py::cipher;
 use std::process;
+use crate::Error;
+use pyo3::exceptions::PyRuntimeError;
+
+impl From<Error> for PyErr {
+    fn from(err: Error) -> PyErr {
+        PyRuntimeError::new_err(format!("Error: {}", err))
+    }
+}
 
 #[pymodule]
 #[pyo3(name = "fractus")]
@@ -10,5 +19,6 @@ fn fractus(m: &Bound<'_, PyModule>) -> PyResult<()> {
     }).expect("Error setting Ctrl+C handler");
 
     let _ = hash(&m);
+    let _ = cipher(&m);
     Ok(())
 }
